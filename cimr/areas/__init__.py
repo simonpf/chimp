6e4f8964a2fe98968205d0bee84394a45de68790
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-07-19 21:33:58 +0200

    Added polygonal ROI.

--- cimr/areas/__init__.py
+++ cimr/areas/__init__.py
@@ -6,8 +6,15 @@ Contains area definitions for the regions used by CIMR.
 """
 from pathlib import Path
 
+import numpy as np
 import pyresample
-from pansat.roi import ROI
+
+from pansat.roi import ROI, PolygonROI
+
+NORDIC_1 = pyresample.load_area(Path(__file__).parent / "cimr_nordic.yml")
+NORDIC_2 = NORDIC_1[(slice(0, None, 2), slice(0, None, 2))]
+NORDIC_4 = NORDIC_2[(slice(0, None, 2), slice(0, None, 2))]
+NORDIC_8 = NORDIC_4[(slice(0, None, 2), slice(0, None, 2))]
 
 ROI_NORDIC = ROI(
     -9.05380216185029,
@@ -16,9 +23,13 @@ ROI_NORDIC = ROI(
     73.3321989854415
 )
 
-NORDIC_1 = pyresample.load_area(Path(__file__).parent / "cimr_nordic.yml")
-NORDIC_2 = NORDIC_1[(slice(0, None, 2), slice(0, None, 2))]
-NORDIC_4 = NORDIC_2[(slice(0, None, 2), slice(0, None, 2))]
-NORDIC_8 = NORDIC_4[(slice(0, None, 2), slice(0, None, 2))]
+_lons, _lats = NORDIC_8.get_lonlats()
+ROI_POLY =  PolygonROI(np.array([
+    [_lons[0, 0], _lats[0, 0]],
+    [_lons[0, -1], _lats[0, -1]],
+    [_lons[-1, -1], _lats[-1, -1]],
+    [_lons[-1, 0], _lats[-1, 0]],
+]))
+
 
 
