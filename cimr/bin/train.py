df5f34ad40c9076fea4fb60b38320bfecbc47c86
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2022-11-26 20:05:05 +0100

    Use quantiles from model.

---
+++ cimr/bin/train.py
@@ -0,0 +1,367 @@
+"""
+==============
+cimr.bin.train
+==============
+
+This sub-module implements the cimr CLI to train the retrieval.
+"""
+from calendar import monthrange
+import logging
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+import importlib
+import multiprocessing as mp
+from pathlib import Path
+import sys
+
+import numpy as np
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def add_parser(subparsers):
+    """
+    Add parser for 'train' command to top-level parser. This function
+    is called from the top-level parser defined in 'cimr.bin'.
+
+    Args:
+        subparsers: The subparsers object provided by the top-level parser.
+    """
+    parser = subparsers.add_parser(
+        "train",
+        help="Train retrieval network.",
+        description=(
+            """
+            Train the retrieval network.
+            """
+        ),
+    )
+    parser.add_argument(
+        "training_data",
+        metavar="training_data",
+        type=str,
+        help="Folder containing the training data.",
+    )
+    parser.add_argument(
+        "model_path",
+        metavar="model_path",
+        type=str,
+        help="Path to store the trained model.",
+    )
+    parser.add_argument(
+        "--validation_data",
+        metavar="path",
+        type=str,
+        help="Folder containing the validation data.",
+        default=None
+    )
+    parser.add_argument(
+        "--n_stages",
+        metavar="N",
+        type=int,
+        default=4,
+        help="Number of stages in encoder/decoder architecture.",
+    )
+    parser.add_argument(
+        "--n_features",
+        metavar="N",
+        type=int,
+        default=128,
+        help="Number of features in first encoder stage.",
+    )
+    parser.add_argument(
+        "--n_blocks",
+        metavar="N",
+        type=int,
+        default=4,
+        help="Number of blocks per encoder stage.",
+    )
+    parser.add_argument(
+        "--batch_size",
+        metavar="N",
+        type=int,
+        default=4,
+        help="The batch size to use during training.",
+    )
+    parser.add_argument(
+        "--lr",
+        metavar="lr",
+        type=float,
+        default=0.0005,
+        help="The learning rate with which to start the training",
+    )
+    parser.add_argument(
+        "--n_epochs",
+        metavar="n_epochs",
+        type=int,
+        default=20,
+        help="The number of epochs to train the model for.",
+    )
+    parser.add_argument(
+        "--accelerator",
+        metavar="device",
+        type=str,
+        default="gpu",
+        help="The accelerator to use for training.",
+    )
+    parser.add_argument(
+        "--sequence_length",
+        metavar="n",
+        type=int,
+        default=1,
+        help="Length of training sequences."
+    )
+    parser.add_argument(
+        "--forecast",
+        metavar="n",
+        type=int,
+        default=None,
+        help="Number of forecast steps to train the model on."
+    )
+    parser.add_argument(
+        "--input_size",
+        metavar="n",
+        type=int,
+        default=256,
+        help="Size of the input scenes at 4 km resolution."
+    )
+    parser.add_argument(
+        "--quality_threshold",
+        metavar="q",
+        type=float,
+        default=0.8,
+        help="Quality index threshold for radar data."
+    )
+    parser.add_argument(
+        "--name",
+        metavar="name",
+        type=str,
+        default=None,
+        help="Name to use for logging."
+    )
+    parser.add_argument(
+        "--freeze_norms",
+        action="store_true",
+        help="Use running statistics in normalization layers."
+    )
+    parser.add_argument(
+        "--freeze_obs_branch",
+        action="store_true",
+        help="Train only temporal stepping and merging modules."
+    )
+    parser.add_argument(
+        "--gradient_clipping",
+        default=None,
+        type=float,
+        help="Threshold value for gradient clipping."
+    )
+    parser.add_argument(
+        "--sources",
+        type=str,
+        nargs="+",
+        default=["visir", "geo", "mw"]
+    )
+    parser.add_argument(
+        "--sample_rate",
+        type=int,
+        default=1
+    )
+
+    parser.set_defaults(func=run)
+
+
+def run(args):
+    """
+    Run training.
+
+    Args:
+        args: The namespace object provided by the top-level parser.
+    """
+    from torch.optim import AdamW, SGD
+    from torch.optim.lr_scheduler import CosineAnnealingLR
+    import pytorch_lightning as pl
+    from pytorch_lightning.callbacks import LearningRateMonitor, Callback
+    from quantnn.qrnn import QRNN
+    from quantnn import metrics
+    from quantnn import transformations
+    from cimr.models import CIMRBaselineV2, CIMRSeqV2
+
+    #
+    # Prepare training and validation data.
+    #
+
+    from cimr.data.training_data import (CIMRSequenceDataset,
+                                         CIMRDataset,
+                                         sparse_collate)
+
+    from torch.utils.data import DataLoader
+
+    training_data = Path(args.training_data)
+    if not training_data.exists():
+        LOGGER.error(
+            "Provided training data path '%s' doesn't exist."
+        )
+        sys.exit()
+
+    if args.sequence_length == 1:
+        training_data = CIMRDataset(
+            training_data,
+            window_size=args.input_size,
+            quality_threshold=args.quality_threshold,
+            sources=args.sources,
+            sample_rate=args.sample_rate
+        )
+    else:
+        training_data = CIMRSequenceDataset(
+            training_data,
+            sequence_length=args.sequence_length,
+            window_size=args.input_size,
+            quality_threshold=args.quality_threshold,
+            forecast=args.forecast,
+            sources=args.sources,
+            sample_rate=args.sample_rate
+        )
+
+    training_loader = DataLoader(
+        training_data,
+        batch_size=args.batch_size,
+        num_workers=16,
+        worker_init_fn=training_data.init_rng,
+        collate_fn=sparse_collate,
+        shuffle=True,
+        pin_memory=True
+    )
+
+    validation_data = args.validation_data
+    validation_loader = None
+    if validation_data is not None:
+        validation_data = Path(validation_data)
+        if not validation_data.exists():
+            LOGGER.error(
+                "Provided validation data path '%s' doesn't exist."
+            )
+            sys.exit()
+        if args.sequence_length == 1:
+            validation_data = CIMRDataset(
+                training_data,
+                window_size=args.input_size,
+                quality_threshold=args.quality_threshold,
+                sources=args.sources
+            )
+        else:
+            validation_data = CIMRSequenceDataset(
+                training_data,
+                sequence_length=args.sequence_length,
+                window_size=args.input_size,
+                quality_threshold=args.quality_threshold
+            )
+
+        validation_loader = DataLoader(
+            validation_data,
+            batch_size=4 * args.batch_size,
+            num_workers=8,
+            worker_init_fn=validation_data.init_rng,
+            shuffle=False,
+            collate_fn=sparse_collate,
+            pin_memory=True
+        )
+
+    #
+    # Create model
+    #
+
+    n_stages = args.n_stages
+    n_blocks = args.n_blocks
+    n_features = args.n_features
+
+    model_path = Path(args.model_path)
+
+    transformations = {
+        "iwc": transformations.LogLinear(),
+        "iwp": transformations.LogLinear()
+    }
+
+    if model_path.exists() and not model_path.is_dir():
+        qrnn = QRNN.load(model_path)
+        model = qrnn.model
+    else:
+        if model_path.is_dir():
+            model_path = model_path / f"cimr_{n_stages}_{n_blocks}_{n_features}.pckl"
+
+        if args.sequence_length == 1:
+            model = CIMRBaselineV2(
+                sources=args.sources,
+            )
+        else:
+            model = CIMRSeqV2(
+                sources=args.sources
+            )
+
+        quantiles = np.linspace(0, 1, 32)
+        quantiles[0] = 1e-3
+        quantiles[-1] = 1 - 1e-3
+
+        qrnn = QRNN(
+            model=model,
+            quantiles=quantiles
+        )
+
+    #
+    # Run training
+    #
+
+    metrics = [
+        metrics.Bias(),
+        metrics.Correlation(),
+        metrics.CRPS(),
+        metrics.MeanSquaredError(),
+        metrics.ScatterPlot(log_scale=True),
+        metrics.CalibrationPlot()
+    ]
+    lm = qrnn.lightning(mask=-100, metrics=metrics, name=args.name)
+    optimizer = AdamW(model.parameters(), lr=args.lr)
+    scheduler = CosineAnnealingLR(optimizer, T_max=args.n_epochs)
+    lm.optimizer = optimizer
+    lm.scheduler = scheduler
+
+    devices = None
+    if args.accelerator in ["cuda", "gpu"]:
+        devices = -1
+
+
+    class FreezeObsBranch(Callback):
+        """
+        Callback class to freeze norm layers.
+        """
+        def on_train_epoch_start(self, trainer, pl_module):
+            pl_module.model.encoder.train(False)
+            pl_module.model.decoder.train(False)
+            pl_module.model.head.train(False)
+
+    callbacks = [LearningRateMonitor()]
+    if args.freeze_norms:
+        callbacks.append(FreezeNorms())
+    if args.freeze_obs_branch:
+        lm.model.encoder.freeze()
+        lm.model.decoder.freeze()
+        lm.model.head.freeze()
+        callbacks.append(FreezeObsBranch())
+
+    trainer = pl.Trainer(
+        max_epochs=args.n_epochs,
+        accelerator=args.accelerator,
+        devices=devices,
+        precision=16,
+        logger=lm.tensorboard,
+        callbacks=callbacks,
+        strategy="ddp",
+        replace_sampler_ddp=True,
+        gradient_clip_val=args.gradient_clipping,
+        enable_checkpointing=False,
+    )
+    trainer.fit(
+        model=lm,
+        train_dataloaders=training_loader,
+        val_dataloaders=validation_loader,
+    )
+    qrnn.save(model_path)
