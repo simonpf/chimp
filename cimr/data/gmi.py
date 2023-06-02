8f22cb917fa3d26408299205c897056461d6846b
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-06-02 20:46:34 +0200

    Make sure obs are stored with channels along first dim.

--- cimr/data/gmi.py
+++ cimr/data/gmi.py
@@ -19,6 +19,7 @@ import xarray as xr
 
 from cimr.utils import round_time
 from cimr.data.utils import make_microwave_band_array
+from cimr.data.resample import resample_tbs
 
 BANDS = {
     "mw_low": {
@@ -85,7 +86,7 @@ def resample_swaths(domain, scene):
     return tbs_r
 
 
-def save_scene(domain, scene, tbs_r, output_folder):
+def save_scene(time, tbs_r, output_folder, time_step):
     """
     Save training data scene.
 
@@ -93,73 +94,39 @@ def save_scene(domain, scene, tbs_r, output_folder):
         scene: xarray.Dataset containing the overpass scene over the
             domain. This data is only used  to extract the meta data of the
             training scene.
-        tbs_low: A dict containing the S1 and S2 swaths resampled to
-            the 8 km grid.
-        tbs_high: A dict containing the S3 and S4 swaths resampled
-            to the 16 km grid.
+        tbs_r: An xarray.Dataset containing the resampled brigthness
+            temperatures.
         output_folder: The folder to which to write the training data.
     """
-    # Only use scenes with substantial information
-    key_8 = list(tbs_r.keys())[-1]
-    if np.any(np.isfinite(tbs_r[key_8][..., 0]), axis=-1).sum() < 100:
-        return None
-
-    # Extract observations and map to CIMR bands.
-    results = {}
-    for band, chans in BANDS.items():
-        obs = make_microwave_band_array(domain, band)
-        for ind_out, (name, ind_in) in chans.items():
-            obs[..., ind_out] = tbs_r[name][..., ind_in]
-        results[band] = obs
-
-    dataset = xr.Dataset()
-    for band in BANDS:
-        if band == "mw_low":
-            dims = ("y_16", "x_16", f"channels_{band}")
-        else:
-            dims = ("y_8", "x_8", f"channels_{band}")
-        dataset[band] = (dims, results[band].data)
-
-    time = scene.scan_time.mean().item()
-    time_15 = round_time(time)
+    minutes = time_step.seconds // 60
+    time_15 = round_time(time, minutes=minutes)
     year = time_15.year
     month = time_15.month
     day = time_15.day
     hour = time_15.hour
     minute = time_15.minute
 
-    filename = f"mw_{year}{month:02}{day:02}_{hour:02}_{minute:02}.nc"
+    filename = f"gmi_{year}{month:02}{day:02}_{hour:02}_{minute:02}.nc"
     output_filename = Path(output_folder) / filename
 
-    if output_filename.exists():
-        dataset_out = xr.load_dataset(output_filename)
-        for band in BANDS:
-            if band in dataset_out:
-                var_out = dataset_out[band]
-                var_in = dataset[band]
-                missing = ~np.isfinite(var_in.data)
-                var_out.data[~missing] = var_in.data[~missing]
-            else:
-                dataset_out[band] = dataset[band]
-        dataset_out.attrs["sensor"] += ", " + scene.attrs["InstrumentName"]
-        dataset_out.attrs["satellite"] += ", " + scene.attrs["SatelliteName"]
-        dataset_out.to_netcdf(output_filename)
-    else:
-        comp = {
-            "dtype": "int16",
-            "scale_factor": 0.01,
-            "zlib": True,
-            "_FillValue": -99,
-        }
-        dataset.attrs["sensor"] = scene.attrs["InstrumentName"]
-        dataset.attrs["satellite"] = scene.attrs["SatelliteName"]
-        encoding = {band: comp for band in BANDS}
-        dataset.to_netcdf(output_filename, encoding=encoding)
+    tbs_r = tbs_r.transpose("channels", "y", "x", "swath_centers")
 
+    comp = {
+        "dtype": "int16",
+        "scale_factor": 0.01,
+        "zlib": True,
+        "_FillValue": -99,
+    }
+    encoding = {
+        "tbs": comp,
+        "swath_center_col_inds": {"dtype": "int16"},
+        "swath_center_row_inds": {"dtype": "int16"}
+    }
+    tbs_r.to_netcdf(output_filename, encoding=encoding)
     return None
 
 
-def process_file(domain, data, output_folder):
+def process_file(domain, data, output_folder, time_step):
     """
     Extract training data from a single GMI L1C file.
 
@@ -169,6 +136,7 @@ def process_file(domain, data, output_folder):
         data: An 'xarray.Dataset' containing the L1C data.
         output_folder: Path to the root of the directory tree to which
             to write the training data.
+        time_step: The time step between consecutive retrieval steps.
     """
     data = data.copy()
     data["latitude"] = data["latitude_s1"]
@@ -177,11 +145,22 @@ def process_file(domain, data, output_folder):
     scenes = find_overpasses(domain["roi"], data)
 
     for scene in scenes:
-        tbs_r = resample_swaths(domain, data)
-        save_scene(domain, scene, tbs_r, output_folder)
+        tbs_r = resample_tbs(domain[8], data, n_swaths=2, radius_of_influence=15e3)
+        time = scene.scan_time.mean().data.item()
+        tbs_r.attrs = scene.attrs
+        save_scene(time, tbs_r, output_folder, time_step)
 
 
-def process_day(domain, year, month, day, output_folder, path=None):
+def process_day(
+        domain,
+        year,
+        month,
+        day,
+        output_folder,
+        path=None,
+        time_step=timedelta(minutes=15)
+
+):
     """
     Extract GMI input observations for the CIMR retrieval.
 
@@ -189,19 +168,19 @@ def process_day(domain, year, month, day, output_folder, path=None):
         domain: A domain dict specifying the area for which to
             extract GMI input data.
         year: The year.
-        month: The month>
+        month: The month.
         day: The day.
         output_folder: The root of the directory tree to which to write
             the training data.
         path: Not used, included for compatibility.
+        time_step: The time step between consecutive retrieval steps.
     """
-    output_folder = Path(output_folder) / "microwave"
+    output_folder = Path(output_folder) / "gmi"
     if not output_folder.exists():
         output_folder.mkdir(parents=True, exist_ok=True)
 
     start_time = datetime(year, month, day)
     end_time = datetime(year, month, day) + timedelta(hours=23, minutes=59)
-    # Iterate over platform.
     provider = GesdiscProvider(l1c_gpm_gmi_r)
     product_files = provider.get_files_in_range(start_time, end_time)
     # For all file on given day.
@@ -214,4 +193,4 @@ def process_day(domain, year, month, day, output_folder, path=None):
                 tmp = Path(tmp)
                 provider.download_file(filename, tmp / filename)
                 data = l1c_gpm_gmi_r.open(tmp / filename)
-                process_file(domain, data, output_folder)
+                process_file(domain, data, output_folder, time_step)
