1d0faa51a2c761dfad77d94b7cc77ef47fb3c630
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2023-09-07 13:26:38 +0200

    Add test for resampling functions.

---
+++ test/test_resample.py
@@ -0,0 +1,53 @@
+import numpy as np
+from pathlib import Path
+import xarray as xr
+
+from cimr.areas import CONUS_4
+from cimr.data.resample import resample_swath_center, resample_tbs
+from cimr.areas import NORDIC
+from cimr.data.gmi import (
+    l1c_gpm_gmi_r,
+)
+
+data_path = Path(__file__).parent / "data"
+gmi_file = "1C-R.GPM.GMI.XCAL2016-C.20200501-S075828-E093100.035075.V07A.HDF5"
+
+
+def test_resample_swath_center():
+    """
+    Ensure that resampling the longitude and latitude coordinates of a
+    rectangular domain returns a vertical line at the center of the domain.
+    """
+
+    lons, lats = CONUS_4.get_lonlats()
+    row_indices, col_indices = resample_swath_center(
+        CONUS_4,
+        lons,
+        lats,
+        radius_of_influence=10e3
+    )
+    assert np.all(col_indices == lats.shape[1] // 2)
+
+    # Ensure that swath outside domain returns empty arrays.
+    lons[:] = 0.0
+    row_indices, col_indices = resample_swath_center(
+        CONUS_4,
+        lons,
+        lats,
+        radius_of_influence=10e3
+    )
+    assert len(row_indices) == 0
+
+
+def test_resample_gmi_tbs():
+    """
+    Test resampling of GMI TBS.
+    """
+    domain = NORDIC
+    product = l1c_gpm_gmi_r
+    data = product.open(data_path / "obs" / gmi_file)
+    tbs_r = resample_tbs(domain[8], data, 2, radius_of_influence=15e3)
+
+    assert tbs_r.channels.size == 13
+    assert np.any(np.isfinite(tbs_r["tbs"]))
+    assert tbs_r.swath_centers.size > 0
