fbf61edacd322e6e5d423d4dc21306ac433baa09
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2023-11-26 11:39:53 -0700

    Make sure goes inputs objects are created.

--- cimr/data/__init__.py
+++ cimr/data/__init__.py
@@ -8,5 +8,6 @@ satellite data sets.
 import cimr.data.gpm
 import cimr.data.cpcir
 import cimr.data.mrms
+import cimr.data.goes
 from cimr.data.input import Input, MinMaxNormalized, get_input
 from cimr.data.reference import ReferenceData, get_reference_data
