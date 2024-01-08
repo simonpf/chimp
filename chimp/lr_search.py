94ea1422f472c83678573a1bdb1521a0c40a0d69
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-01-07 19:45:33 -0700

    Ensure that compute config is parsed.

--- chimp/lr_search.py
+++ chimp/lr_search.py
@@ -117,6 +117,8 @@ def cli(
     module = LightningRetrieval(retrieval_model, "retrieval_module", training_schedule)
 
     compute_config = read_compute_config(LOGGER, model_path, compute_config)
+    if compute_config is not None:
+        compute_config = ComputeConfig.parse(compute_config)
 
     run_lr_search(
         module,
