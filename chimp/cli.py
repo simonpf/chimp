e0609d0275a7604770d399e5f1e42b8eae858cc2
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-01-17 14:59:47 -0700

    Add process command to new click interface.

--- chimp/cli.py
+++ chimp/cli.py
@@ -9,6 +9,7 @@ import click
 from chimp import training
 from chimp import eda
 from chimp import lr_search
+from chimp import processing
 from chimp.data import extract
 
 
@@ -23,3 +24,4 @@ chimp.command(name="lr_search")(lr_search.cli)
 chimp.command(name="eda")(eda.cli)
 chimp.command(name="train")(training.cli)
 chimp.command(name="extract_data")(extract.cli)
+chimp.command(name="process")(processing.cli)
--- chimp/processing.py
+++ chimp/processing.py
@@ -5,45 +5,22 @@ chimp.processing
 Routines for the processing of retrievals and forecasts.
 """
 import logging
+from pathlib import Path
+from typing import List
 
+import click
 import numpy as np
-from quantnn.packed_tensor import PackedTensor
-from quantnn.quantiles import posterior_mean, sample_posterior, probability_larger_than
-from quantnn.models.pytorch.lightning import to_device
-from quantnn.mrnn import Quantiles, Density, MSE, Classification
 import torch
 from torch import nn
 import xarray as xr
+from pytorch_retrieve.architectures import load_model
 
 from chimp.tiling import Tiler
+from chimp.data.input import InputDataset
 
 
 LOGGER = logging.getLogger(__file__)
 
-
-def get_observation_mask(model_input, upsample=1):
-    """
-    Get a mask of valid observations.
-
-    Args:
-        x: A 4D ``torch.Tensor`` containing network of a given
-            observation type.
-        upsample: Factor by which the input should be upsampled.
-
-    Return:
-        A 3D, spatial mask that masks pixels that have valid observation
-        input.
-    """
-    if isinstance(model_input, PackedTensor):
-        model_input = model_input.tensor
-
-    while upsample > 1:
-        model_input = nn.functional.interpolate(
-            model_input, scale_factor=2, mode="nearest"
-        )
-        upsample = upsample // 2
-
-    return (model_input > -1.3).any(1)
 
 
 def empty_input(model, model_input):
@@ -69,7 +46,7 @@ def empty_input(model, model_input):
 
 
 def retrieval_step(
-    model, model_input, state, tile_size=256, device="cuda", float_type=torch.float32
+    model, model_input,  tile_size=256, device="cuda", float_type=torch.float32
 ):
     """
     Run retrieval on given input.
@@ -77,70 +54,30 @@ def retrieval_step(
     Args:
         model: The CHIMP model to perform the retrieval with.
         model_input: A dict containing the input for the given time step.
-        state: The current hidden state of the model.
         tile_size: The size to use for the tiling.
 
     Return:
-        An ``xarray.Dataset`` containing the retrieval results and the
-        crreutn internal state of the retrieval.
+        An ``xarray.Dataset`` containing the retrieval results.
     """
 
     x = model_input
-    x = {name: tensor.to(dtype=float_type, device=device) for name, tensor in x.items()}
-    tiler = Tiler(x, tile_size=tile_size, overlap=64)
+    x = {name: tensor[None].to(dtype=float_type, device=device) for name, tensor in x.items()}
+    tiler = Tiler(x, tile_size=tile_size, overlap=32)
 
     means = {}
 
-    output_names = list(model.losses.keys())
-
-    model.model.eval()
-    model.model.to(device=device, dtype=float_type)
+    model = model.to(device=device, dtype=float_type).eval()
 
     def predict_fun(x_t):
         results = {}
 
         with torch.no_grad():
             with torch.autocast(device_type=device, dtype=float_type):
-                y_pred = model.model(x_t)
-                if isinstance(y_pred, PackedTensor):
-                    y_pred = y_pred._t
-
+                y_pred = model(x_t)
                 for key, y_pred_k in y_pred.items():
-                    if key in model.transformation:
-                        transform = model.transformation[key]
-                        y_pred_k = transform.invert(y_pred_k)
-
-                    loss = model.losses[key]
-
-                    if isinstance(loss, (Quantiles, Density, MSE)):
-                        y_mean_k = loss.posterior_mean(y_pred=y_pred_k)
-                        p_non_zero = y_mean_k
-                        if hasattr(loss, "probability_larger_than"):
-                            p_non_zero = loss.probability_larger_than(
-                                y_pred_k,
-                                1e-2,
-                            )
-
-                        p_heavy = y_mean_k
-                        if hasattr(loss, "probability_larger_than"):
-                            p_heavy = loss.probability_larger_than(
-                                y_pred_k,
-                                1e1,
-                            )
-
-                        results[key + "_mean"] = y_mean_k.float().cpu().numpy()[0]
-                        results["p_" + key + "_non_zero"] = (
-                            p_non_zero.float().cpu().numpy()[0]
-                        )
-                        results["p_" + key + "_heavy"] = (
-                            p_heavy.float().cpu().numpy()[0]
-                        )
-
-                    elif isinstance(loss, Classification):
-                        classes = loss.predict(y_pred_k)
-                        results[key + "_prob"] = classes.cpu().numpy()[0]
-
-            return results
+                    y_mean_k = y_pred_k.expected_value()[0, 0]
+                    results[key + "_mean"] = y_mean_k.cpu().numpy()
+        return results
 
     dims = ("classes", "y", "x")
     results = tiler.predict(predict_fun)
@@ -150,89 +87,23 @@ def retrieval_step(
     return results
 
 
-def make_forecast(
-    qrnn,
-    dataset,
-    forecast_time,
-    obs_steps,
-    forecast_steps,
-):
-    """
-    Make a precipitation forecasts for a given start time.
+@click.argument("model")
+@click.argument("path")
+@click.argument("input_datasets", nargs=-1)
+@click.argument("output_path")
+@click.option("--device", type=str, default="cuda")
+def cli(
+        model: Path,
+        path: Path,
+        input_datasets: List[str],
+        output_path: Path,
+        device: str = "cuda"
+) -> int:
 
-    Args:
-        qrnn: The QRNN to use to make the forecast.
-        dataset: The SingleStepDataset providing the input data.
-        forecast_time: The time for which to perform the forecast.
-        obs_steps: Number of observations to ingest prior to making the
-            forecast.
-        forecast_steps: The number of steps to forecast.
-
-    Return:
-        A dataset containing the forecast.
-    """
-    state = None
-
-    inputs = dataset.get_forecast_input(forecast_time, obs_steps)
-    if inputs is None:
-        return None
-
-    LOGGER.info("Processing %s observations.", len(inputs))
-
-    with torch.no_grad():
-        for x, y, *_ in inputs:
-            _, state = qrnn.model.forward(x, state=state, return_state=True)
-
-    slice_y = inputs[0][2]
-    slice_x = inputs[0][3]
-    quantiles = torch.tensor(qrnn.quantiles)
-
-    f_state = state
-    y_pred_mean = []
-    y_pred_sampled = []
-    y_pred_prob = []
-
-    LOGGER.info("Running forecast.")
-
-    with torch.no_grad():
-        for _ in range(forecast_steps):
-            y_pred, f_state = qrnn.model(None, state=f_state, return_state=True)
-
-            # Posterior mean.
-            y_mean = (
-                posterior_mean(y_pred=y_pred, quantile_axis=1, quantiles=quantiles)
-                .cpu()
-                .numpy()[0, slice_y, slice_x]
-            )
-            y_pred_mean.append(y_mean)
-
-            # P(dbz >= -5)
-            prob = (
-                probability_larger_than(
-                    y_pred=y_pred, y=-5, quantile_axis=1, quantiles=quantiles
-                )
-                .cpu()
-                .numpy()[0, slice_y, slice_x]
-            )
-            y_pred_prob.append(prob)
-
-            # Sample from posterior
-            sampled = (
-                sample_posterior(y_pred=y_pred, quantile_axis=1, quantiles=quantiles)
-                .cpu()
-                .numpy()[0, 0, slice_y, slice_x]
-            )
-            y_pred_sampled.append(sampled)
-
-    y_pred_mean = np.stack(y_pred_mean)
-    y_pred_sampled = np.stack(y_pred_sampled)
-    y_pred_prob = np.stack(y_pred_prob)
-
-    results = xr.Dataset(
-        {
-            "dbz_mean": (("steps", "y", "x"), y_pred_mean),
-            "dbz_prob": (("steps", "y", "x"), y_pred_prob),
-            "dbz_sampled": (("steps", "y", "x"), y_pred_sampled),
-        }
-    )
-    return results
+    input_data = InputDataset(path, input_datasets)
+    model = load_model(model)
+    output_path = Path(output_path)
+    for time, model_input in input_data:
+        results = retrieval_step(model, model_input, tile_size=128, device=device)
+        results["time"] = time
+        results.to_netcdf(output_path / f"results_{time}.nc")
