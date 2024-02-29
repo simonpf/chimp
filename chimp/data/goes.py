aff14391a3205aa39b24dfbc670f6f5fc2e086cf
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-02-29 07:34:56 -0700

    Refactor GOES data interface.

--- chimp/data/goes.py
+++ chimp/data/goes.py
@@ -1,8 +1,8 @@
 """
 chimp.data.goes
-==============
+===============
 
-Functionality for reading and processing GOES data.
+Defines CHIMP input datasets for observations from the GOES 16, 17, and 18 satellites.
 """
 from datetime import datetime, timedelta
 import logging
@@ -15,55 +15,48 @@ import pandas as pd
 from pansat.roi import any_inside
 from pansat.time import TimeRange, to_datetime64
 from pansat.download.providers import GOESAWSProvider
-from pansat.products.satellite.goes import GOES16L1BRadiances, GOES17L1BRadiances
+from pansat.products.satellite.goes import (
+    GOES16L1BRadiances,
+    GOES17L1BRadiances,
+    GOES18L1BRadiances
+)
 from pyresample import geometry, kd_tree
 from satpy import Scene
 import xarray as xr
 
 from chimp import areas
 from chimp.utils import round_time
-from chimp.data.input import Input, MinMaxNormalized
+from chimp.data.input import InputDataset
+from chimp.data.utils import get_output_filename
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_output_filename(time):
-    """
-    Get filename for training sample.
 
-    Args:
-        time: The observation time.
-
-    Return:
-        A string specifying the filename of the training sample.
-    """
-    time_15 = round_time(time)
-    year = time_15.year
-    month = time_15.month
-    day = time_15.day
-    hour = time_15.hour
-    minute = time_15.minute
-
-    filename = f"goes_{year}{month:02}{day:02}_{hour:02}_{minute:02}.nc"
-    return filename
+CHANNEL_CONFIGURATIONS = {
+    "ALL": list(range(1, 17)),
+    "MATCHED": [
+        2,   # 0.635 um
+        3,   # 0.81 um
+        5,   # 1.64 um
+        7,   # 3.92 um
+        8,   # 6.25 um
+        10,  # 7.35 um
+        11,  # 8.7 um
+        12,  # 9.66 um
+        13,  # 10.3 um
+        15,  # 12 um
+        16,  # 13.4 um
+    ]
+}
 
 
-goes_channels = [ # Seviri match
-    2,   # 0.635 um
-    3,   # 0.81 um
-    5,   # 1.64 um
-    7,   # 3.92 um
-    8,   # 6.25 um
-    10,  # 7.35 um
-    11,  # 8.7 um
-    12,  # 9.66 um
-    13,  # 10.3 um
-    15,  # 12 um
-    16,  # 13.4 um
-]
-
-
-def download_and_resample_goes_data(time, domain):
+def download_and_resample_goes_data(
+        time: datetime,
+        domain: "Area",
+        product_class: type,
+        channel_configuration: str = "ALL"
+):
     """
     Download GOES data closest to a given requested time step, resamples it
     and returns the result as an xarray dataset.
@@ -76,56 +69,55 @@ def download_and_resample_goes_data(time, domain):
         An xarray dataset containing the GOES observations resampled to the
         CONUS_4 domain.
     """
-    channel_names = [f"C{channel:02}" for channel in goes_channels]
-    with TemporaryDirectory() as tmp:
+    channels = CHANNEL_CONFIGURATIONS[channel_configuration]
+    channel_names = [f"C{channel:02}" for channel in channels]
 
-        goes_files = []
-        for band in goes_channels:
-            prod = GOES16L1BRadiances("F", band)
-            time_range = TimeRange(
-                to_datetime64(time) - np.timedelta64(7 * 60, "s"),
-                to_datetime64(time) + np.timedelta64(7 * 60, "s"),
-            )
-            channel_files = prod.find_files(time_range)
+    goes_files = []
 
-            if len(channel_files) == 0:
-                return None
+    for band in channels:
+        prod = product_class("F", band)
+        time_range = TimeRange(
+            to_datetime64(time) - np.timedelta64(7 * 60, "s"),
+            to_datetime64(time) + np.timedelta64(7 * 60, "s"),
+        )
+        recs = prod.get(time_range)
 
-            file_inds = TimeRange(time, time).find_closest_ind(
-                [rec.temporal_coverage for rec in channel_files]
-            )
-            goes_files.append(
-                channel_files[file_inds[0]].download(destination=tmp)
-            )
+        if len(recs) == 0:
+            return None
+
+        file_inds = TimeRange(time, time).find_closest_ind(
+            [rec.temporal_coverage for rec in recs]
+        )
+        goes_files.append(recs[file_inds[0]])
+
+    scene = Scene([str(rec.local_path) for rec in goes_files], reader="abi_l1b")
+    scene.load(channel_names)
+    scene = scene.resample(domain)
+    data = scene.to_xarray_dataset().compute()
+
+    obs_refl = []
+    obs_therm = []
+
+    for channel in channels:
+        ch_name = f"C{channel:02}"
+        if channel <= 6:
+            obs_refl.append(data[ch_name].data)
+        else:
+            obs_therm.append(data[ch_name].data)
+    obs_refl = np.stack(obs_refl, -1)
+    obs_therm = np.stack(obs_therm, -1)
+
+    obs = xr.Dataset({
+        "refls": (("y", "x", "channels_refl"), obs_refl),
+        "tbs": (("y", "x", "channels_therm"), obs_therm)
+    })
+    start_time = data.attrs["start_time"]
+    d_t = data.attrs["end_time"] - data.attrs["start_time"]
+    obs.attrs["time"] = to_datetime64(start_time + 0.5 * d_t)
+    return  obs
 
 
-        scene = Scene([str(rec.local_path) for rec in goes_files], reader="abi_l1b")
-        scene.load(channel_names)
-        scene = scene.resample(areas.CONUS_4)
-        data = scene.to_xarray_dataset().compute()
-
-        tbs_refl = []
-        for ch_name in channel_names[:3]:
-            tbs_refl.append(data[ch_name].data)
-        tbs_refl = np.stack(tbs_refl, -1)
-
-        tbs_therm = []
-        for ch_name in channel_names[3:]:
-            tbs_therm.append(data[ch_name].data)
-        tbs_therm = np.stack(tbs_therm, -1)
-
-        tbs = xr.Dataset({
-            "refls": (("y", "x", "channels_refl"), tbs_refl),
-            "tbs": (("y", "x", "channels_therm"), tbs_therm)
-        })
-        start_time = data.attrs["start_time"]
-        d_t = data.attrs["end_time"] - data.attrs["start_time"]
-        tbs.attrs["time"] = to_datetime64(start_time + 0.5 * d_t)
-
-        return  tbs
-
-
-def save_file(dataset, output_folder):
+def save_file(dataset, output_folder, filename):
     """
     Save file to training data.
 
@@ -136,7 +128,6 @@ def save_file(dataset, output_folder):
 
     """
     dataset = dataset.copy()
-    filename = get_output_filename(dataset.attrs["time"].item())
     output_filename = Path(output_folder) / filename
     dataset.attrs["time"] = str(dataset.attrs["time"])
     encoding = {}
@@ -159,9 +150,25 @@ def save_file(dataset, output_folder):
     dataset.to_netcdf(output_filename, encoding=encoding)
 
 
-class GOESInputData(Input, MinMaxNormalized):
-    def __init__(self):
-        super().__init__("goes", 4, ["refls", "tbs"])
+class GOES(InputDataset):
+    """
+    Input data class for GOES observations.
+
+    This class represents satellite observations from any of the recent GOES satellites.
+    """
+    def __init__(
+            self, series: str, config: str, product_class: type
+    ):
+        self.series = series
+        self.config = config
+        self.product_class = product_class
+        if config.upper() == "ALL":
+            dataset_name = "goes_" + series.lower()
+            input_name = "goes"
+        else:
+            dataset_name = "goes_" + series.lower() + "_" + config.lower()
+            input_name = "goes_" + config.lower()
+        super().__init__(dataset_name, input_name, 4, ["refls", "tbs"])
         self.n_channels = 11
 
     def process_day(
@@ -190,25 +197,28 @@ class GOESInputData(Input, MinMaxNormalized):
             time_step: The temporal resolution of the training data.
             include_scan_time: Not used.
         """
-        output_folder = Path(output_folder) / "goes"
+        output_folder = Path(output_folder) / self.dataset_name
         if not output_folder.exists():
             output_folder.mkdir(parents=True, exist_ok=True)
-
-        existing_files = [
-            f.name for f in output_folder.glob(f"goes_{year}{month:02}{day:02}*.nc")
-        ]
 
         start_time = datetime(year, month, day)
         end_time = datetime(year, month, day) + timedelta(hours=23, minutes=59)
         time = start_time
         while time < end_time:
-
-            output_filename = get_output_filename(time)
-            if not (output_folder / output_filename).exists():
-                dataset = download_and_resample_goes_data(time, domain[self.scale])
-                save_file(dataset, output_folder)
-
+            dataset = download_and_resample_goes_data(
+                time,
+                domain[self.scale],
+                self.product_class,
+                self.config
+            )
+            filename = get_output_filename(self.input_name, time, minutes=time_step)
+            save_file(dataset, output_folder, filename)
             time = time + time_step
 
 
-goes = GOESInputData()
+goes_16 = GOES("16", "ALL", GOES16L1BRadiances)
+goes_17 = GOES("17", "ALL", GOES17L1BRadiances)
+goes_18 = GOES("18", "ALL", GOES18L1BRadiances)
+goes_16_matched = GOES("16", "MATCHED", GOES16L1BRadiances)
+goes_17_matched = GOES("17", "MATCHED", GOES17L1BRadiances)
+goes_18_matched = GOES("18", "MATCHED", GOES18L1BRadiances)
