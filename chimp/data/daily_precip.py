c1510157d1c16ca312eba2343d2d217871a95bff
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-03-09 07:53:54 -0700

    Refactored 'daily_precip' reference data.

--- chimp/data/daily_precip.py
+++ chimp/data/daily_precip.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import List
+from typing import Optional, List
 
 import numpy as np
 import pandas as pd
@@ -19,9 +19,11 @@ from pansat.products.satellite import persiann, gpm
 
 from pansat.time import TimeRange
 
-from pansat.time import to_datetime64
+from pansat.time import to_datetime64, to_datetime
+from pansat.geometry import Geometry
 import xarray as xr
 
+from chimp.areas import Area
 from chimp.utils import round_time
 from chimp.data.reference import ReferenceDataset, RetrievalTarget
 from chimp.data.utils import get_output_filename
@@ -32,6 +34,9 @@ LOGGER = logging.getLogger(__name__)
 
 
 precip = RetrievalTarget("precipitation", None)
+
+
+SPLIT = datetime(2000, 6, 1)
 
 
 class DailyPrecip(ReferenceDataset):
@@ -52,92 +57,122 @@ class DailyPrecip(ReferenceDataset):
         super().__init__("daily_precip", scale, [precip], None)
 
 
-    def process_day(
+    def find_files(
             self,
-            domain,
-            year,
-            month,
-            day,
-            output_folder,
-            path=None,
-            time_step=timedelta(minutes=15),
-            include_scan_time=False
-    ):
+            start_time: np.datetime64,
+            end_time: np.datetime64,
+            time_step: np.timedelta64,
+            roi: Optional[Geometry] = None,
+            path: Optional[Path] = None
+    ) -> List[Path]:
         """
-        Extract daily precipitation accumulations.
+        Find input data files within a given file range from which to extract
+        training data.
 
         Args:
-            year: The year
-            month: The month
-            day: The day
-            output_folder: The folder to which to write the extracted
-                observations.
-            path: Not used, included for compatibility.
-            time_step: Time step defining the temporal resolution at which to extract
-                training samples.
-            include_scan_time: Ignored. Included for compatibility.
+            start_time: Start time of the time range.
+            end_time: End time of the time range.
+            time_step: The time step of the retrieval.
+            roi: An optional geometry object describing the region of interest
+                that can be used to restriced the file selection a priori.
+            path: If provided, files should be restricted to those available from
+                the given path.
+
+        Return:
+            A list of locally available files to extract CHIMP training data from.
         """
-        output_folder = Path(output_folder) / "daily_precip"
-        if not output_folder.exists():
-            output_folder.mkdir(parents=True, exist_ok=True)
+        if path is not None:
+            path = Path(path)
+            all_files = sorted(list(path.glob("**/*")))
+            matching = []
+            matching += [
+                path for path in all_files
+                if persiann.cdr_daily.matches(path)
+                and persiann.cdr_daily.get_temporal_coverage(path).start < SPLIT - timedelta(hours=1)
+                and persiann.cdr_daily.get_temporal_coverage(path).covers(TimeRange(start_time, end_time))
+            ]
+            matching += [
+                path for path in all_files
+                if gpm.l3b_day_3imerg_ms_mrg_v07.matches(path)
+                and gpm.l3b_day_3imerg_ms_mrg_v07.get_temporal_coverage(path).start >= SPLIT
+                and gpm.l3b_day_3imerg_ms_mrg_v07.get_temporal_coverage(path).covers(TimeRange(start_time, end_time))
 
-        start_time = datetime(year, month, day)
-        end_time = datetime(year, month, day) + timedelta(hours=24)
-        time = start_time
-        files = []
-        while time < end_time:
-            if time < datetime(2000, 6, 1):
-                recs = persiann.cdr_daily.find_files(TimeRange(time, time))
-                if len(recs) == 0:
-                    LOGGER.warning(
-                        "Didn't find any PERSIANN files for %s.",
-                        time
-                    )
-                    time = time + time_step
-                    continue
+            ]
+            return matching
 
-                rec = recs[0].get()
-                data = persiann.cdr_daily.open(rec)
-            else:
-                recs = gpm.l3b_day_3imerg_ms_mrg_v07.find_files(TimeRange(time, time))
-                if len(recs) == 0:
-                    LOGGER.warning(
-                        "Didn't find any IMERG files for %s.",
-                        time
-                    )
-                    time = time + time_step
-                    continue
+        matching = []
 
-                rec = recs[0].get()
-                data = gpm.l3b_day_3imerg_ms_mrg_v07.open(rec)
-                data["surface_precipitation"].attrs = {}
-                data["latitude"].attrs = {}
-                data["longitude"].attrs = {}
-                data["time"].attrs = {}
-                data = data.rename(surface_precipitation="precipitation")
+        start_time = to_datetime(start_time)
+        end_Time = to_datetime(end_time)
 
-            if isinstance(domain, dict):
-                domain = domain[self.scale]
+        print("LSTART :: ", min(end_time, SPLIT))
+        print("RSTART :: ", max(start_time, SPLIT))
+        if start_time < SPLIT:
+            matching += [
+                rec.get().local_path for rec in persiann.cdr_daily.get(
+                    TimeRange(start_time, min(end_time, SPLIT - timedelta(hours=1)))
+                )
+            ]
+        if end_time > SPLIT:
+            matching += [
+                rec.get().local_path for rec in gpm.l3b_day_3imerg_ms_mrg_v07.get(
+                    TimeRange(max(start_time, SPLIT), end_time)
+                )
+            ]
+        return matching
 
-            lons, lats = domain.get_lonlats()
-            lons = lons[0]
-            lats = lats[:, 0]
-            data = data.interp(latitude=lats, longitude=lons)[{"time": 0}]
+    def process_file(
+            self,
+            path: Path,
+            domain: Area,
+            output_folder: Path,
+            time_step: np.timedelta64
+    ):
+        """
+        Extract training samples from a given source file.
 
-            output_filename = get_output_filename(
-                "precip", time, time_step.total_seconds() // 60
-            )
-            encoding = {
-                "latitude": {"dtype": "float32"},
-                "longitude": {"dtype": "float32"},
-                "precipitation": {"dtype": "float32"},
-            }
-            data.to_netcdf(
-                output_folder / output_filename,
-                encoding=encoding
-            )
+        Args:
+           path: A Path object pointing to the file to process.
+           domain: An area object defining the training domain.
+           output_folder: A path pointing to the folder to which to write
+               the extracted training data.
+           time_step: A timedelta object defining the retrieval time step.
+        """
+        output_folder = Path(output_folder) / self.name
+        output_folder.mkdir(exist_ok=True)
 
-            time = time + time_step
+        if persiann.cdr_daily.matches(path):
+            data = persiann.cdr_daily.open(path)
+            time = persiann.cdr_daily.get_temporal_coverage(path).start
+        else:
+            data = gpm.l3b_day_3imerg_ms_mrg_v07.open(path)
+            time = gpm.l3b_day_3imerg_ms_mrg_v07.get_temporal_coverage(path).start
+            data["surface_precipitation"].attrs = {}
+            data["latitude"].attrs = {}
+            data["longitude"].attrs = {}
+            data["time"].attrs = {}
+            data = data.rename(surface_precipitation="precipitation")
+
+        if isinstance(domain, Area):
+            domain = domain[self.scale]
+
+        lons, lats = domain.get_lonlats()
+        lons = lons[0]
+        lats = lats[:, 0]
+        data = data.interp(latitude=lats, longitude=lons)[{"time": 0}]
+
+        output_filename = get_output_filename(
+            "precip", time, time_step
+        )
+        encoding = {
+            "latitude": {"dtype": "float32"},
+            "longitude": {"dtype": "float32"},
+            "precipitation": {"dtype": "float32"},
+        }
+        data.to_netcdf(
+            output_folder / output_filename,
+            encoding=encoding
+        )
 
 
-daily_precip = DailyPrecip(1)
+DAILY_PRECIP = DailyPrecip(1)
