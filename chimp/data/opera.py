21e041cb88bae443bd1c8d8e1cd292197f03887a
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-03-10 10:48:00 -0600

    Refactored PATMOS-x input dataset.

--- chimp/data/opera.py
+++ chimp/data/opera.py
@@ -8,7 +8,7 @@ Defines the OPERA input data classes for loading precipitation and
 from datetime import datetime, timedelta
 from pathlib import Path
 import re
-from typing import Optional
+from typing import List, Optional
 
 from h5py import File
 import numpy as np
@@ -17,14 +17,14 @@ from pyproj import Transformer
 import xarray as xr
 
 from pansat import TimeRange, Product
+from pansat.geometry import Geometry
 from pansat.products.ground_based.opera import reflectivity, surface_precip
 
 from chimp.areas import Area
 from chimp.data import ReferenceDataset
 from chimp.data.reference import RetrievalTarget
-from chimp.utils import round_time
 from chimp.data.resample import resample_data
-from chimp.data.utils import get_output_filename
+from chimp.data.utils import round_time, get_output_filename
 
 
 class Opera(ReferenceDataset):
@@ -43,76 +43,99 @@ class Opera(ReferenceDataset):
             "opera_" + self.target_name,
             scale=4,
             targets=[RetrievalTarget(target_name)],
-            quality_index="quality_indicator"
+            quality_index="qi_indicator"
         )
 
-    def process_day(
-        self,
-        domain: dict,
-        year: int,
-        month: int,
-        day: int,
-        output_folder: Path,
-        path: Path = Optional[None],
-        time_step: timedelta = timedelta(minutes=15),
-        include_scan_time=False,
-    ):
+    def find_files(
+            self,
+            start_time: np.datetime64,
+            end_time: np.datetime64,
+            time_step: np.timedelta64,
+            roi: Optional[Geometry] = None,
+            path: Optional[Path] = None
+    ) -> List[Path]:
         """
-        Extract OPERA reference data observations for the CHIMP retrieval.
+        Find input data files within a given file range from which to extract
+        training data.
 
         Args:
-            domain: A domain dict specifying the area for which to
-                extract OPERA data.
-            year: The year.
-            month: The month.
-            day: The day.
-            output_folder: The root of the directory tree to which to write
-                the training data.
-            path: Not used, included for compatibility.
-            time_step: The time step between consecutive retrieval steps.
-            include_scan_time: Not used; included for compatibility.
+            start_time: Start time of the time range.
+            end_time: End time of the time range.
+            time_step: The time step of the retrieval.
+            roi: An optional geometry object describing the region of interest
+                that can be used to restriced the file selection a priori.
+            path: If provided, files should be restricted to those available from
+                the given path.
+
+        Return:
+            A list of locally available files to extract CHIMP training data from.
         """
+        prod = self.pansat_product
+        if path is not None:
+            path = Path(path)
+            all_files = sorted(list(path.glob("**/*.nc")))
+            matching = [
+                    path for path in all_files if prod.matches(path)
+                    and prod.get_temporal_coverage(path).covers(TimeRange(time_start, time_end))
+            ]
+            return matching
+
+        recs = prod.get(TimeRange(start_time, end_time))
+        return [rec.local_path for rec in recs]
+
+    def process_file(
+        self,
+        path: Path,
+        domain: Area,
+        output_folder: Path,
+        time_step: np.timedelta64
+    ):
+        """
+        Extract training samples from a given input data file.
+
+        Args:
+            path: A Path object pointing to the file to process.
+            domain: An area object defining the training domain.
+            output_folder: A path pointing to the folder to which to write
+                the extracted training data.
+            time_step: A timedelta object defining the retrieval time step.
+        """
+
         output_folder = Path(output_folder) / self.name
-        if not output_folder.exists():
-            output_folder.mkdir(parents=True, exist_ok=True)
+        output_folder.mkdir(exist_ok=True)
 
-        start_time = datetime(year, month, day, second=1)
-        end_time = datetime(year, month, day) + timedelta(hours=23, minutes=58)
-        time_range = TimeRange(start_time, end_time)
-
+        prod = self.pansat_product
+        data = prod.open(path)
+        start_time, end_time = prod.get_temporal_coverage(path)
+        start_time = round_time(start_time, time_step)
+        end_time = round_time(end_time, time_step)
         time = start_time
+        while (time < end_time):
 
-        if isinstance(domain, Area):
-            domain = domain[self.scale]
+            data_t = data.interp(time=time, method="nearest")
+            data_interp = data.interp(time=time)
 
-        file_recs = self.pansat_product.get(time_range)
-        if len(file_recs) == 0:
-            raise RuntimeError(f"Didn't find any OPERA files for {year}-{month}-{day}.")
-        if len(file_recs) > 1:
-            raise RuntimeError(
-                f"Found more that one OPERA file for {year}-{month}-{day}."
-            )
+            if "reflectivity" in data_interp:
+                invalid = data_interp.reflectivity.data < -9e6
+                data_interp.reflectivity.data[invalid] = np.nan
+                noise = data_interp.reflectivity.data < -30
+                data_interp.reflectivity.data[noise] = -30
 
-        opera_data = self.pansat_product.open(file_recs[0])
-
-        while time < end_time:
-
-            opera_data_interp = opera_data.interp(time=time)
-
-            if "reflectivity" in opera_data_interp:
-                invalid = opera_data_interp.reflectivity.data < -9e6
-                opera_data_interp.reflectivity.data[invalid] = np.nan
-                noise = opera_data_interp.reflectivity.data < -30
-                opera_data_interp.reflectivity.data[noise] = -30
-
-            data_r = resample_data(opera_data_interp, domain, radius_of_influence=4e3)
-            data_r = data_r.drop_vars(["latitude", "longitude"])
+            data_r = resample_data(data_interp, domain[4], radius_of_influence=4e3)
             data_r = data_r.rename({"quality_indicator": "qi"})
 
             output_filename = get_output_filename(
-                self.target_name, time, minutes=time_step.total_seconds() // 60
+                self.target_name, time, time_step
             )
+
             encoding = {
+                "reflectivity": {
+                    "dtype": "uint16",
+                    "add_offset": 0,
+                    "scale_factor": 0.01,
+                    "_FillValue": -1,
+                    "zlib": True,
+                },
                 "reflectivity": {
                     "dtype": "uint8",
                     "add_offset": -30,
@@ -128,9 +151,11 @@ class Opera(ReferenceDataset):
                     "_FillValue": 255,
                 },
             }
+            encoding = {name: encoding[name] for name in data_r.variables}
             data_r.to_netcdf(output_folder / output_filename, encoding=encoding)
+
             time = time + time_step
 
 
-opera = Opera("reflectivity", reflectivity)
-opera = Opera("surface_precip", surface_precip)
+OPERA_REFLECTIVITY = Opera("reflectivity", reflectivity)
+OPERA_SURFACE_PRECIP = Opera("surface_precip", surface_precip)
