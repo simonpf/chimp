71c287d46933f3faee0856d3b675dcbff5284228
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-03-26 16:24:41 -0600

    Fix tests for CPCIR data.

--- test/data/test_cpcir.py
+++ test/data/test_cpcir.py
@@ -8,6 +8,7 @@ from conftest import NEEDS_PANSAT_PASSWORD
 
 import numpy as np
 import pytest
+import xarray as xr
 
 from chimp.areas import NORDICS
 from chimp.data.cpcir import CPCIR
@@ -29,12 +30,17 @@ def test_process_file(tmp_path):
     files = CPCIR.find_files(
         "2019-01-01T00:00:00",
         "2019-01-01T00:00:00",
-        np.timedelta64(15, "m"),
+        np.timedelta64(30, "m"),
     )
 
     CPCIR.process_file(
         files[0],
         NORDICS,
         tmp_path,
-        np.timedelta64(15, "m")
+        np.timedelta64(30, "m")
     )
+
+    training_files = sorted(list((tmp_path / "cpcir").glob("*.nc")))
+    assert len(training_files) == 2
+    data = xr.load_dataset(training_files[0])
+    assert (data.tbs.data > 0).sum() > 100
