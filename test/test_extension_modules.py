b69813e26888b03eb2299772c62c978cc8b626cd
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-07-25 02:16:54 -0600

    Fix module name collision.

--- test/test_extension_modules.py
+++ test/test_extension_modules.py
@@ -14,7 +14,7 @@ from chimp.data import InputDataset
 
 class Test(InputDataset):
     def __init__(self):
-        super().__init__("test_dataset", "test", 4, "tbs")
+        super().__init__("input_dataset", "test", 4, "tbs")
 
 test = Test()
     """
@@ -23,7 +23,7 @@ test = Test()
 
 @pytest.fixture
 def input_dataset_extension_module(tmp_path):
-    with open(tmp_path / "test_input.py", "w") as mod_file:
+    with open(tmp_path / "test_input_extension.py", "w") as mod_file:
         mod_file.write(INPUT_DATASET_MODULE)
     return tmp_path
 
@@ -34,10 +34,9 @@ def test_input_dataset_extension(monkeypatch, input_dataset_extension_module):
     to the list of available inputs.
     """
     monkeypatch.syspath_prepend(input_dataset_extension_module)
-    monkeypatch.setenv("CHIMP_EXTENSION_MODULES", "test_input")
+    monkeypatch.setenv("CHIMP_EXTENSION_MODULES", "test_input_extension")
 
-    extensions.load()
-    input_dataset = get_input_dataset("test_dataset")
+    input_dataset = get_input_dataset("input_dataset")
     assert input_dataset is not None
 
 
@@ -47,7 +46,7 @@ from chimp.data import ReferenceDataset
 
 class Test(ReferenceDataset):
     def __init__(self):
-        super().__init__("test_dataset", 4, [])
+        super().__init__("reference_dataset", 4, [])
 
 test = Test()
 print("IMPORTED EXTENSION")
@@ -70,5 +69,5 @@ def test_reference_dataset_extension(monkeypatch, reference_dataset_extension_mo
     monkeypatch.syspath_prepend(reference_dataset_extension_module)
     monkeypatch.setenv("CHIMP_EXTENSION_MODULES", "test_reference")
 
-    reference_dataset = get_reference_dataset("test_dataset")
+    reference_dataset = get_reference_dataset("reference_dataset")
     assert reference_dataset is not None
